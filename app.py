247539e8d27635e2118a828425411193f57649a4
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-20 18:27:39 +0300

    logging

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from aiohttp_jinja2 import setup as setup_jinja2
 from aiohttp_swagger import setup_swagger
 from jinja2 import FileSystemLoader
 import os
+import logging
 
 BASE_PATH = os.path.dirname(__file__)
 
 app = web.Application()
+
+
 setup_routes(app)
 setup_jinja2(app, loader=FileSystemLoader(BASE_PATH + '/templates'))
 setup_swagger(app, swagger_url="/api/v1/doc", ui_version=2)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    web.run_app(app)
+    web.run_app(app, access_log_format='%r %s %b %t "%a"')
--- views/Index.py
+++ views/Index.py
@@ -1,6 +1,5 @@
 from aiohttp.web import View
 from aiohttp import web
-import json
 
 
 class IndexView(View):
@@ -18,18 +17,8 @@ class IndexView(View):
                 description: json response with  `status`
         """
         response_obj = {
-            'status': 'success'
+            'status': 'success',
+            'headers': str(self.request.headers)
         }
-        headers = {
-            'content-type': 'application/json'
-        }
-        return web.Response(
-            text=json.dumps(
-                response_obj,
-                sort_keys=True,
-                indent=4
-            ),
-            status=200,
-            headers=headers
-        )
+        return web.json_response(response_obj)
 
