f75e2c4c2361ee4dadd2d0464e68c113ded0a3b4
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-22 12:46:50 +0300

    * added personal profile create and update view

--- views/profiles.py
+++ views/profiles.py
@@ -1,14 +1,53 @@
 from aiohttp import web
 from aiohttp_jwt import check_permissions, match_any, login_required
 from models import Profile
-from schemas import profile_schema
+from schemas import profile_schema, my_profile_schema
 from db import (
     get_objects,
     get_object_by_id,
+    get_object_by_user_id,
     update_object_by_id,
+    update_object_by_user_id,
     insert_object,
     delete_object_by_id
 )
+
+
+class MyProfileView(web.View):
+    @login_required
+    async def get(self):
+        async with self.request.app['db'].acquire() as conn:
+            user_id = self.request['user']['user_id']
+            profile = await get_object_by_user_id(conn, Profile, user_id)
+            result = my_profile_schema.dump(profile)
+            return web.json_response(result)
+
+    @login_required
+    async def post(self):
+        async with self.request.app['db'].acquire() as conn:
+            if self.request.content_type == 'application/json':
+                data = await self.request.json()
+            else:
+                data = await self.request.post()
+            validated_data = my_profile_schema.load(data)
+            validated_data['user_id'] = self.request['user']['user_id']
+            profile = await insert_object(conn, Profile, validated_data)
+            result = my_profile_schema.dump(profile)
+            return web.json_response(result, status=201)
+
+    @login_required
+    async def patch(self):
+        async with self.request.app['db'].acquire() as conn:
+            if self.request.content_type == 'application/json':
+                data = await self.request.json()
+            else:
+                data = await self.request.post()
+            user_id = self.request['user']['user_id']
+            validated_data = my_profile_schema.load(data, partial=True)
+            validated_data['user_id'] = user_id
+            profile = await update_object_by_user_id(conn, Profile, user_id, validated_data)
+            result = my_profile_schema.dump(profile)
+            return web.json_response(result)
 
 
 class ProfilesListView(web.View):
