e7994dc265a8e13fbb0fe75e78760cecf88cd922
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-22 12:42:09 +0300

    * added jwt auth middleware * added error type to error message

--- middlewares.py
+++ middlewares.py
@@ -3,10 +3,25 @@ from aiohttp import web
 from exceptions import BadRequest, RecordNotFound
 from json import JSONDecodeError
 from marshmallow import ValidationError
+from aiohttp_jwt import JWTMiddleware
+from settings import JWT_SECRET_KEY
 
 
 async def handle_http_error(request, e, status):
-    return web.json_response({'message': str(e)}, status=status)
+    return web.json_response(
+        {
+            'message':  f'{type(e).__name__}: {str(e)}'
+        },
+        status=status
+    )
+
+
+jwt_middleware = JWTMiddleware(
+    secret_or_pub_key=JWT_SECRET_KEY,
+    request_property='user',
+    algorithms=['HS256'],
+    credentials_required=False
+)
 
 
 @middleware
@@ -14,18 +29,15 @@ async def error_middleware(request, handler):
     try:
         return await handler(request)
     except web.HTTPException as e:
-        return await handle_http_error(request, str(e), status=e.status)
+        return await handle_http_error(request, e, status=e.status)
     except (BadRequest, JSONDecodeError, ValidationError) as e:
-        return await handle_http_error(request, str(e), status=400)
+        return await handle_http_error(request, e, status=400)
     except RecordNotFound as e:
-        return await handle_http_error(request, str(e), status=404)
+        return await handle_http_error(request, e, status=404)
     except Exception as e:
-        return await handle_http_error(
-            request,
-            f'{type(e).__name__}: {str(e)}',
-            status=500
-        )
+        return await handle_http_error(request, e, status=500)
 
 
 def setup_middlewares(app):
     app.middlewares.append(error_middleware)
+    app.middlewares.append(jwt_middleware)
