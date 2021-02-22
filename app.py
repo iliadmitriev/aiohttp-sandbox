a99ef211e4c27a78aabd9c5c63073ff7d44ec98d
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-22 12:39:50 +0300

    run app reading from settings

--- app.py
+++ app.py
@@ -3,15 +3,12 @@ from routes import setup_routes
 from aiohttp_jinja2 import setup as setup_jinja2
 from aiohttp_swagger import setup_swagger
 from jinja2 import FileSystemLoader
-import pathlib
 import sys
 import logging
 
 from db import setup_db
 from middlewares import setup_middlewares
-from settings import access_log_format
-
-BASE_PATH = pathlib.Path(__file__).parent
+from settings import access_log_format, BASE_PATH, APP_PORT, APP_HOST
 
 
 async def init_app(argv=None):
@@ -31,7 +28,9 @@ def main(argv):
     app = init_app(argv)
     web.run_app(
         app,
-        access_log_format=access_log_format
+        access_log_format=access_log_format,
+        host=APP_HOST,
+        port=APP_PORT
     )
 
 
