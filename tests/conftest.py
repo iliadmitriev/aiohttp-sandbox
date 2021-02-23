0c115185edd5d8d80d24fd59acf0b1c64f115f0e
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-23 19:08:03 +0300

    view tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,56 +1,71 @@
-import pytest
 import pathlib
 import sys
 import time
-from aiohttp import web
 import jwt
+from sqlalchemy import create_engine
+import pytest
+
+from models import Base
 
 BASE_PATH = pathlib.Path(__file__).parent.parent
 sys.path.append(BASE_PATH)
 
 
-@pytest.fixture
-def fixture_setup_app():
-    from routes import setup_routes
-    from middlewares import setup_middlewares
-    from db import setup_db
-    app = web.Application()
-    setup_routes(app)
-    setup_middlewares(app)
-    setup_db(app)
-    return app
+def create_test_db():
+    import secrets
+    import psycopg2
+    from psycopg2 import sql
+    from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+    from settings import dsn
+    test_db_name = f'test_{secrets.token_hex(nbytes=10)}'
+
+    con = psycopg2.connect(dsn=dsn)
+
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+    cur = con.cursor()
+    cur.execute(sql.SQL("CREATE DATABASE {}").format(
+        sql.Identifier(test_db_name))
+    )
+    cur.close()
+    return test_db_name
 
 
-@pytest.fixture
-def cli(loop, aiohttp_client, fixture_setup_app):
-    """
-    fixtures loop, aiohttp_client - loaded from pytest-aiohttp
-    """
-    return loop.run_until_complete(aiohttp_client(fixture_setup_app))
+def setup_test_db_dsn():
+    test_db = create_test_db()
+    from settings import conf
+    test_db_dsn = f'{conf["engine"]}://{conf["user"]}:{conf["password"]}'\
+        f'@{conf["host"]}:{conf["port"]}/{test_db}'
+    return test_db_dsn
 
 
-@pytest.fixture
-def admin_access_token():
+def create_and_migrate_test_db_dsn():
+    test_db_dsn = setup_test_db_dsn()
+    engine = create_engine(test_db_dsn)
+    Base.metadata.create_all(engine)
+    return test_db_dsn
+
+
+def get_admin_access_token(user_id=100):
     from settings import JWT_SECRET_KEY
     payload = {
         'token_type': 'access',
         # expires in 300 seconds = 5 min
         'exp': int(time.time() + 300),
         'username': 'admin',
-        'user_id': 100,
+        'user_id': user_id,
         'scope': 'admin'
     }
     return jwt.encode(payload=payload, key=JWT_SECRET_KEY, algorithm='HS256')
 
 
-@pytest.fixture
-def unprivileged_access_token():
+def get_unprivileged_access_token(user_id=200):
     from settings import JWT_SECRET_KEY
     payload = {
         'token_type': 'access',
         # expires in 300 seconds = 5 min
         'exp': int(time.time() + 300),
         'username': 'common-user',
-        'user_id': 200,
+        'user_id': user_id,
     }
     return jwt.encode(payload=payload, key=JWT_SECRET_KEY, algorithm='HS256')
--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -1,58 +1,321 @@
-from views.profiles import MyProfileView
+from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
+from aiohttp import web
+from db import insert_object
+from schemas import profile_schema
+from views.profiles import Profile
+import json
+
+from tests.conftest import (
+    get_admin_access_token,
+    create_and_migrate_test_db_dsn,
+    get_unprivileged_access_token
+)
 
 
-class TestProfileListAdmin:
-    async def test_profiles_list_view_401_unauthorized(self, cli):
-        resp = await cli.get('/profiles')
+class AioHTTPTestCaseWithTestDB(AioHTTPTestCase):
+    dsn = create_and_migrate_test_db_dsn()
+    admin_access_token = get_admin_access_token(user_id=100)
+    unprivileged_access_token = get_unprivileged_access_token(user_id=200)
+    unprivileged_access_token_404 = get_unprivileged_access_token(user_id=404)
+
+    async def get_application(self):
+        from routes import setup_routes
+        from middlewares import setup_middlewares
+        from db import setup_db
+        app = web.Application()
+        setup_routes(app)
+        setup_middlewares(app)
+        setup_db(app, self.dsn)
+        self.app = app
+        return app
+
+
+class TestProfileListAdmin(AioHTTPTestCaseWithTestDB):
+
+    @unittest_run_loop
+    async def test_profiles_list_view_401_unauthorized(self):
+        resp = await self.client.get('/profiles')
         assert resp.status == 401
 
-    async def test_profiles_list_view_200_with_token(self, cli, admin_access_token):
+    @unittest_run_loop
+    async def test_profiles_list_view_200_with_token(self):
         headers = {
-            'Authorization': f'Bearer {admin_access_token}'
+            'Authorization': f'Bearer {self.admin_access_token}'
         }
-        resp = await cli.get('/profiles', headers=headers)
-        print(resp)
+        resp = await self.client.get('/profiles', headers=headers)
         assert resp.status == 200
 
-    def test_profiles_list_view_create_201(self):
-        assert True
+    @unittest_run_loop
+    async def test_profiles_list_view_create_201(self):
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}'
+        }
+        data = {
+            'firstname': 'ivan',
+            'surname': 'petrov',
+            'birthdate': '1990-02-04',
+            'user_id': 2000
+        }
+        resp = await self.client.post('/profiles', headers=headers, data=data)
+        assert resp.status == 201
 
-    def test_profiles_list_view_create_400(self):
-        assert True
+    async def test_profiles_list_view_create_json_201(self):
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'firstname': 'ivan',
+            'surname': 'petrov',
+            'birthdate': '1990-02-04',
+            'user_id': 1777
+        }
+        resp = await self.client.post(
+            '/profiles', headers=headers,
+            data=json.dumps(data)
+        )
+        assert resp.status == 201
+
+    @unittest_run_loop
+    async def test_profiles_list_view_create_400(self):
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}'
+        }
+        data = {
+            'user_id': 2000
+        }
+        resp = await self.client.post('/profiles', headers=headers, data=data)
+        assert resp.status == 400
 
 
-class TestProfileDetailAdmin:
-    def test_profiles_detail_view_200(self):
-        assert True
+class TestProfileDetailAdmin(AioHTTPTestCaseWithTestDB):
 
-    def test_profiles_detail_view_404(self):
-        assert True
+    @unittest_run_loop
+    async def test_profiles_detail_view_200(self):
+        async with self.app['db'].acquire() as conn:
+            validated_data = profile_schema.load({
+                'firstname': 'ivan',
+                'surname': 'petrov',
+                'birthdate': '1990-02-04',
+                'user_id': 2001
+            })
+            profile = await insert_object(conn, Profile, validated_data)
+            pr = profile_schema.dump(profile)
 
-    def test_profiles_detail_put_200(self):
-        assert True
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}'
+        }
+        resp = await self.client.get(f'/profiles/{pr["id"]}', headers=headers)
+        assert resp.status == 200
 
-    def test_profiles_detail_patch_200(self):
-        assert True
+    @unittest_run_loop
+    async def test_profiles_detail_view_404(self):
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}'
+        }
+        resp = await self.client.get('/profiles/1999', headers=headers)
+        assert resp.status == 404
 
-    def test_profiles_detail_delete_200(self):
-        assert True
+    @unittest_run_loop
+    async def test_profiles_detail_put_200(self):
+        async with self.app['db'].acquire() as conn:
+            validated_data = profile_schema.load({
+                'firstname': 'ivan',
+                'surname': 'petrov',
+                'birthdate': '1990-02-04',
+                'user_id': 2002
+            })
+            profile = await insert_object(conn, Profile, validated_data)
+            pr = profile_schema.dump(profile)
+
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}'
+        }
+        data = {
+                'firstname': 'ivan',
+                'surname': 'petrov',
+                'birthdate': '1990-02-04',
+                'user_id': 2003
+        }
+        resp = await self.client.put(f'/profiles/{pr["id"]}', headers=headers, data=data)
+        assert resp.status == 200
+
+    @unittest_run_loop
+    async def test_profiles_detail_patch_200(self):
+        async with self.app['db'].acquire() as conn:
+            validated_data = profile_schema.load({
+                'firstname': 'ivan',
+                'surname': 'smirnov',
+                'birthdate': '1990-02-04',
+                'user_id': 2004
+            })
+            profile = await insert_object(conn, Profile, validated_data)
+            pr = profile_schema.dump(profile)
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}'
+        }
+        data = {
+                'birthdate': '1994-02-04',
+        }
+        resp = await self.client.patch(f'/profiles/{pr["id"]}', headers=headers, data=data)
+        assert resp.status == 200
+
+    @unittest_run_loop
+    async def test_profiles_detail_delete_200(self):
+        async with self.app['db'].acquire() as conn:
+            validated_data = profile_schema.load({
+                'firstname': 'ivan',
+                'surname': 'smirnov',
+                'birthdate': '1990-02-04',
+                'user_id': 2005
+            })
+            profile = await insert_object(conn, Profile, validated_data)
+            pr = profile_schema.dump(profile)
+        headers = {
+            'Authorization': f'Bearer {self.admin_access_token}'
+        }
+        resp = await self.client.delete(f'/profiles/{pr["id"]}', headers=headers)
+        assert resp.status == 200
 
 
-class TestMyProfileUnprivileged:
-    def test_my_profile_view_200(self):
-        assert True
+class TestMyProfileUnprivileged(AioHTTPTestCaseWithTestDB):
+    @unittest_run_loop
+    async def test_my_profile_view_404(self):
+        headers = {
+            'Authorization': f'Bearer {self.unprivileged_access_token_404}'
+        }
+        resp = await self.client.get('/my-profile', headers=headers)
+        assert resp.status == 404
 
-    def test_my_profile_view_404(self):
-        assert True
+    @unittest_run_loop
+    async def test_my_profile_create(self):
+        headers = {
+            'Authorization': f'Bearer {self.unprivileged_access_token}'
+        }
+        data = {
+            'firstname': 'user',
+            'surname': 'unprivileged',
+            'birthdate': '1991-05-08'
+        }
+        resp = await self.client.post('/my-profile', headers=headers, data=data)
+        assert resp.status == 201
 
-    def test_my_profile_create(self):
-        assert True
+    @unittest_run_loop
+    async def test_my_profile_create_post_json(self):
+        headers = {
+            'Authorization': f'Bearer {get_unprivileged_access_token(user_id=16)}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'firstname': 'user',
+            'surname': 'unprivileged',
+            'birthdate': '1991-05-08'
+        }
+        resp = await self.client.post(
+            '/my-profile',
+            headers=headers,
+            data=json.dumps(data)
+        )
+        assert resp.status == 201
 
-    def test_my_profile_create_exist_400(self):
-        assert True
+    @unittest_run_loop
+    async def test_my_profile_view_200(self):
+        headers = {
+            'Authorization': f'Bearer {self.unprivileged_access_token}'
+        }
+        resp = await self.client.get('/my-profile', headers=headers)
+        assert resp.status == 200
 
-    def test_my_profile_create_validation_400(self):
-        assert True
+    @unittest_run_loop
+    async def test_my_profile_create_exist_400(self):
+        token = get_unprivileged_access_token(user_id=55)
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        data = {
+            'firstname': 'user',
+            'surname': 'unprivileged',
+            'birthdate': '1991-05-08'
+        }
+        resp = await self.client.post('/my-profile', headers=headers, data=data)
+        assert resp.status == 201
+        token = get_unprivileged_access_token(user_id=55)
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        data = {
+            'firstname': 'user',
+            'surname': 'unprivileged',
+            'birthdate': '1991-05-08'
+        }
+        resp = await self.client.post('/my-profile', headers=headers, data=data)
+        assert resp.status == 400
 
-    def test_my_profile_patch(self):
-        assert True
+    @unittest_run_loop
+    async def test_my_profile_create_validation_400(self):
+        token = get_unprivileged_access_token(user_id=77)
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        data = {
+            'firstname': 'user',
+            'surname': 'unprivileged',
+            'birthdate': 'invalid',
+            'gender': 'invalid'
+        }
+        resp = await self.client.post('/my-profile', headers=headers, data=data)
+        assert resp.status == 400
+
+    @unittest_run_loop
+    async def test_my_profile_patch(self):
+        token = get_unprivileged_access_token(user_id=88)
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        data = {
+            'firstname': 'user',
+            'surname': 'unprivileged'
+        }
+        resp = await self.client.post('/my-profile', headers=headers, data=data)
+        assert resp.status == 201
+        token = get_unprivileged_access_token(user_id=88)
+        headers = {
+            'Authorization': f'Bearer {token}'
+        }
+        data = {
+            'birthdate': '1990-01-01',
+            'gender': 'male'
+        }
+        resp = await self.client.patch('/my-profile', headers=headers, data=data)
+        assert resp.status == 200
+
+    @unittest_run_loop
+    async def test_my_profile_patch_post_json(self):
+        token = get_unprivileged_access_token(user_id=177)
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'firstname': 'user',
+            'surname': 'unprivileged'
+        }
+        resp = await self.client.post(
+            '/my-profile', headers=headers,
+            data=json.dumps(data)
+        )
+        assert resp.status == 201
+        token = get_unprivileged_access_token(user_id=88)
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'birthdate': '1990-01-01',
+            'gender': 'male'
+        }
+        resp = await self.client.patch(
+            '/my-profile', headers=headers,
+            data=json.dumps(data)
+        )
+        assert resp.status == 200
