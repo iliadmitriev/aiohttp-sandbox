3017142b7590316704c620e6a091ca9a2f1463e8
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-26 15:20:36 +0300

    disabled test routes

--- routes.py
+++ routes.py
@@ -1,15 +1,15 @@
-from views.render import RenderView
-from views.index import IndexView
-from views.ping import PingView
-from views.jinja import JinjaView
+# from views.render import RenderView
+# from views.index import IndexView
+# from views.ping import PingView
+# from views.jinja import JinjaView
 from views.profiles import ProfilesListView, ProfilesDetailView, MyProfileView
 
 
 def setup_routes(app):
-    app.router.add_view('/jinja', JinjaView)
-    app.router.add_view('/ping', PingView)
-    app.router.add_view('/', IndexView)
-    app.router.add_view('/render', RenderView)
+    # app.router.add_view('/jinja', JinjaView)
+    # app.router.add_view('/ping', PingView)
+    # app.router.add_view('/', IndexView)
+    # app.router.add_view('/render', RenderView)
     app.router.add_view('/profiles', ProfilesListView)
     app.router.add_view('/profiles/{profile_id}', ProfilesDetailView)
     app.router.add_view('/my-profile', MyProfileView)
